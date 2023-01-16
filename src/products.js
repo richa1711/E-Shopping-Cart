8b771b2d094e7d51f799c1e684e9655f4c9a5195
apple <apple@apples-MacBook-Pro.local>
2023-01-16 12:39:31 +0530

    first commit

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/1.jpg";
+import product2 from "./assets/products/2.jpg";
+import product3 from "./assets/products/3.jpg";
+import product4 from "./assets/products/4.jpg";
+import product5 from "./assets/products/5.jpg";
+import product6 from "./assets/products/6.jpg";
+import product7 from "./assets/products/7.jpg";
+import product8 from "./assets/products/8.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Leather Shoes",
+    price: 7800.00,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Denim Jeans",
+    price: 999.00,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Sky Bag",
+    price: 1300.00,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Apple Smart Watch",
+    price: 56900.00,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "IPhone",
+    price: 69000.00,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "DSLR Camera",
+    price: 78900.00,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Apple Macbook Pro",
+    price: 133900.00,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Apple Keyboard",
+    price: 34000.00,
+    productImage: product8,
+  }
+];
